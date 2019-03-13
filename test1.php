51a22edb7bb0773a854ccac7444070ae1620f60e
Kaneka <kaneka.ky@gmail.com>
2019-03-13 04:51:11 -0700

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,33 @@
+<?php
+$data = "Stuff you want encrypted";
+$key = "Secret passphrase used to encrypt your data";
+$cipher = "MCRYPT_SERPENT_256";
+$mode = "MCRYPT_MODE_CBC";
+function encrypt($data, $key, $cipher, $mode) {
+// Encrypt data
+return (string)
+            base64_encode
+                (
+                mcrypt_encrypt
+                    (
+                    $cipher,
+                    substr(($key),0,mcrypt_get_key_size($cipher, $mode)),
+                    $data,
+                    $mode,
+                    substr(($key),0,mcrypt_get_block_size($cipher, $mode))
+                    )
+                );
+}
+function decrypt($data, $key, $cipher, $mode) {
+// Decrypt data
+    return (string)
+            mcrypt_decrypt
+                (
+                $cipher,
+                substr(($key),0,mcrypt_get_key_size($cipher, $mode)),
+                base64_decode($data),
+                $mode,
+                substr(($key),0,mcrypt_get_block_size($cipher, $mode))
+                );
+}
+?>
